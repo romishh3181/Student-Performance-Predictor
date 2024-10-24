68ffcc2eb1cbf511be1385643316013a3600de88
romishh3181 <mavrohan2004@gmail.com>
2024-10-25 00:02:54 +0530

    Prediction Pipeline and Flask app completion

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,7 @@ import sys
 import pandas as pd
 import numpy as np
 import dill
+import pickle
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
 from src.exception import CustomException
@@ -32,3 +33,10 @@ def evaluate_model(X_train,Y_train,X_test,Y_test,models,param):
         return report  
     except Exception as e:
         raise CustomException(e,sys)  
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
